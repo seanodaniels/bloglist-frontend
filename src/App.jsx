75d4472c14dac9549dd07021cc11bd56789c4084
Sean O'Daniels <sodaniels@gmail.com>
2023-12-12 15:15:13 -0500

    Solution for exercise 5.3 and 5.4. Added ability to add blog list via logged in user. Added appropriate notifications and errors.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
+import ErrorMessage from './components/ErrorMessage'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [likes, setLikes] = useState('')
   const [user, setUser] = useState(null)
 
   const handleLogin = async (event) => {
@@ -26,18 +34,30 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      setNotificationMessage(`${user.username} logged in.`)
+      setTimeout(() => {
+        setNotificationMessage(null)
+      }, 5000)
+  
     } catch (exception) {
-      console.log('error: wrong credentials')
+      setErrorMessage('wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
   }
 
   const handleLogout = (event) => {
     event.preventDefault()
+    setNotificationMessage(`${user.username} logged out.`)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
 
     window.localStorage.removeItem('loggedBloglistUser')
     setUser(null)
     setUsername('')
-    setPassword('')
+    setPassword('')    
   }
 
   useEffect(() => {
@@ -54,6 +74,22 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value)
+  }
+
+  const handleLikesChange = (event) => {
+    setLikes(event.target.value)
+  }
 
   const loginForm = () => {
     return (
@@ -90,9 +126,17 @@ const App = () => {
         <p>
           {user.name} logged in 
         </p>
-          <form onSubmit={handleLogout}>
-            <button type="submit">logout</button>
-          </form>
+        <form onSubmit={handleLogout}>
+          <button type="submit">logout</button>
+        </form>
+
+        <form onSubmit={addBloglist}>
+          title: <input value={title} onChange={handleTitleChange} /><br />
+          author: <input value={author} onChange={handleAuthorChange} /><br />
+          url: <input value={url} onChange={handleUrlChange} /><br />
+          <button type="submit">save</button>
+        </form>        
+
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
@@ -100,8 +144,33 @@ const App = () => {
     )
   }
 
+  const addBloglist = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+      likes: 0,
+    }
+
+    blogService.create(blogObject)
+      .then(o => {
+        setBlogs(blogs.concat(o))
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+        setLikes('')
+        setNotificationMessage(`a new blog "${o.title}" by ${o.author} added`)
+      })
+      .catch(error => {
+        console.log('CREATE ERROR:', error)
+      })
+  }
+
   return (
     <div>
+      <Notification message={notificationMessage} />
+      <ErrorMessage message={errorMessage} />
       <h2>blogs</h2>
 
       {user === null ?
