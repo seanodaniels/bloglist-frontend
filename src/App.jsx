91ffda0bcc854be9626e9719b6c06c056a6d77de
Sean O'Daniels <sodaniels@gmail.com>
2023-12-12 13:32:22 -0500

    Solution for exercise 5.1: bloglist frontend, step1

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,7 @@ const App = () => {
   const bloglistForm = () => {
     return (
       <div>
+        <p>{user.name} logged in</p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
